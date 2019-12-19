59c70a1f389e02491b6a6698f9c2ecd254ebfd96
Charles Lampman <Charles.Lampman@outlook.com>
2019-12-18 20:26:27 -0600

    Add Exception.

---
+++ QueryForDapper.Postgres/Exceptions/JoinManyLeftRightEqualException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryForDapper.Postgres.Exceptions
+{
+    public class JoinManyLeftRightEqualException : Exception
+    {
+        public const string ERRROR_MESSAGE = "The type {0} was passed for both the Left and Right table";
+        public JoinManyLeftRightEqualException(Type tableType) : base(String.Format(ERRROR_MESSAGE, tableType.Name)) { }
+    }
+}
